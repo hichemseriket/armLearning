ca35bcef703454e69b09bee6c73ec7418caf2628
hichemseriket <serikethichem@hotmail.fr>
2021-06-02 13:26:47 +0200

    premier vrai contact avec le code de luc, j'ai ajouté le tracé en ligne de l'historique des position et je trouve que le tracking fonctionne mieux que sur le tuto l42project

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ cv2.namedWindow("finish")
 vc = cv2.VideoCapture(0)
 width = vc.get(3)
 height = vc.get(4)
-if vc.isOpened(): # try to get the first frame
+if vc.isOpened():  # try to get the first frame
     rval, frame = vc.read()
     frame2 = frame
 else:
@@ -24,33 +24,32 @@ else:
 xa = random.randint(0, width)
 ya = random.randint(0, height)
 
-lower_green = np.array([ 75, 100, 50])
-upper_green = np.array([ 94, 255,  255])
-color_info=(0, 0, 255)
+lower_green = np.array([75, 100, 50])
+upper_green = np.array([94, 255, 255])
+color_info = (0, 0, 255)
 while rval:
     rval, frame = vc.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    frame = cv2.circle(frame,(xa, ya), 5, (0,0,255), -1)
+    frame = cv2.circle(frame, (xa, ya), 5, (0, 0, 255), -1)
     # preparing the mask to overlay
-    hsv=cv2.blur(hsv, (5, 5))
-    mask = cv2.inRange(hsv,lower_green, upper_green)
+    hsv = cv2.blur(hsv, (5, 5))
+    mask = cv2.inRange(hsv, lower_green, upper_green)
     mask = cv2.erode(mask, None, iterations=1)
     mask = cv2.dilate(mask, None, iterations=1)
-    result = cv2.bitwise_and(frame,frame,mask = mask) 
-    elements  = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+    result = cv2.bitwise_and(frame, frame, mask=mask)
+    elements = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
     if len(elements) > 0:
-        c=max(elements, key=cv2.contourArea)
-        ((x, y), radius)=cv2.minEnclosingCircle(c)
-        if radius>30:
-            print(x, y , 'x, y')
+        c = max(elements, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        if radius > 30:
+            print(x, y, 'x, y')
             cv2.circle(frame, (int(x), int(y)), 5, color_info, 10)
-            
+
     cv2.imshow("preview", frame)
     cv2.imshow("result", result)
     key = cv2.waitKey(20)
-    if key == 27: # exit on ESC
+    if key == 27:  # exit on ESC
         break
 
 cv2.destroyAllWindows()
-cap.release()
-
+vc.release()
